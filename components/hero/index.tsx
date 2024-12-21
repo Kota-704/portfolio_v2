ef2f2a182e35036fd05e149071fb390f20a432a1
Kota-704 <kktt14590358@gmail.com>
2024-12-22 00:47:05 +0900

    ♻️ /画像からテキストへ変更

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,17 +1,17 @@
-import Image from 'next/image';
-
 export default function Hero() {
   return (
-    <section
-      className="hero flex justify-center items-center h-screen bg-contain bg-center bg-no-repeat opacity-30 absolute top-0 left-0 w-full z--1"
-      style={{
-        backgroundImage: 'url(/img/hero_background.png)',
-      }}
-    >
-      <div className="hero-content text-center flex flex-col justify-center items-center">
-        <Image src="/img/logo.png" alt="logo" width={384} height={384} className="z-10" />
-        <p className="text-2xl z-10">Welcome to my portfolio site</p>
+    <>
+      <section
+        className="hero flex justify-center items-center h-screen bg-contain bg-center bg-no-repeat opacity-30 absolute top-0 left-0 w-full z--1"
+        style={{
+          backgroundImage: "url(/img/hero_background.png)",
+        }}
+      ></section>
+      <div className="hero-content text-center flex flex-col justify-center items-center h-screen">
+        <h2 className="text-6xl text-white font-relation">
+          Kota&apos;s Portfolio
+        </h2>
       </div>
-    </section>
+    </>
   );
 }
