e1dbefe9fdf832c317e5a2c3ed39b67e57c467ca
Kota-704 <kktt14590358@gmail.com>
2024-12-22 15:11:07 +0900

    ✨ /フェードインアニメーション追加

---
+++ hooks/useIntersectionObserver.ts
@@ -0,0 +1,31 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+
+export const useIntersectionObserver = (threshold: number = 0.1) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold }
+    );
+
+    const currentRef = ref.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [threshold]);
+
+  return { ref, isVisible };
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,9 +14,29 @@ export default {
       },
       fontFamily: {
         relation: ['"relation-one"', "sans-serif"],
+        "dm-serif": ['"DM Serif Text"', "serif"],
+        bodoni: ['"Bodoni Moda"', "cursive"],
+      },
+      fontWeight: {
+        regular: "400",
       },
       zIndex: {
         "-1": "-1",
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(80px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
       },
     },
   },
