62b2bd2e130c45debaf040ae459d56cf967d78c8
Kota-704 <kktt14590358@gmail.com>
2024-12-24 00:30:33 +0900

    ♻️ /画像サイズ微修正

--- components/SkillItem/index.tsx
+++ components/SkillItem/index.tsx
@@ -8,7 +8,7 @@ interface SkillItemProps {
 export function SkillItem({ image, text }: SkillItemProps) {
   return (
     <li className="flex items-center gap-2">
-      <Image src={`/img/${image}`} alt={text} width={50} height={50} />
+      <Image src={`/img/${image}`} alt={text} width={100} height={100} />
       <p className="text-gray-700 text-sm">{text}</p>
     </li>
   );
