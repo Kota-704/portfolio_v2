fd81c25237281890d28669d266edc34659e5c175
Kota-704 <kktt14590358@gmail.com>
2025-01-30 23:15:30 +0900

    ✨ /フッターの追加

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "../components/hero";
 import Works from "../components/works";
 import { Skill } from "@/components/skill";
 import { Contact } from "@/components/contact";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
         <Skill />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 }
