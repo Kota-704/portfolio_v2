b9ef1d0811bfc6563bd9f6c077cc8bbd74b50d09
Kota-704 <kktt14590358@gmail.com>
2025-02-02 01:08:51 +0900

    ♻️ /ブログページレイアウト修正

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,25 @@
 import { getBlogList } from "@/lib/api";
+import Header from "@/components/header";
 import { Blog } from "@/lib/types";
+import Footer from "@/components/footer";
 
 export default async function BlogPage() {
   try {
     const blogs = await getBlogList();
 
     return (
-      <div>
-        <h1>Blog List</h1>
-        <ul>
-          {blogs.contents.map((blog: Blog) => (
-            <li key={blog.id}>{blog.title}</li>
-          ))}
-        </ul>
-      </div>
+      <>
+        <Header />
+        <div>
+          <h1>Blog List</h1>
+          <ul>
+            {blogs.contents.map((blog: Blog) => (
+              <li key={blog.id}>{blog.title}</li>
+            ))}
+          </ul>
+        </div>
+        <Footer />
+      </>
     );
   } catch (error) {
     console.error("Error fetching blog list:", error);
