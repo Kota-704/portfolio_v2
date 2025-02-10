aecca1c069313c5d348f82bd0609aa5c9eb8a9e4
Kota-704 <kktt14590358@gmail.com>
2025-02-10 23:07:11 +0900

    ♻️ /ボタンの位置微修正

--- components/hamburgerMenu/index.tsx
+++ components/hamburgerMenu/index.tsx
@@ -14,20 +14,20 @@ export default function HamburgerMenu() {
     <>
       <button
         onClick={toggleMenu}
-        className="hamburger absolute top-6 right-8 z-50 lg:hidden h-4 w-6 cursor-pointer"
+        className="hamburger fixed top-6 right-5 z-50 lg:hidden h-4 w-8 cursor-pointer"
       >
         <span
-          className={`absolute h-px w-8 bg-white transition-transform duration-300 ${
+          className={`absolute h-px w-8 left-0 bg-white transition-transform duration-300 ${
             isOpen ? "rotate-45 top-2" : "top-0"
           }`}
         ></span>
         <span
-          className={`absolute h-px w-8 bg-white transition-opacity duration-300 ${
+          className={`absolute h-px w-8 left-0 bg-white transition-opacity duration-300 ${
             isOpen ? "opacity-0" : "top-2"
           }`}
         ></span>
         <span
-          className={`absolute h-px w-8 bg-white transition-transform duration-300 ${
+          className={`absolute h-px w-8 left-0 bg-white transition-transform duration-300 ${
             isOpen ? "-rotate-45 top-2" : "top-4"
           }`}
         ></span>
